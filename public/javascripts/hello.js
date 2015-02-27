fece55408ba8acda8e06f60b8b7bf72e42b061c5
Arpit <arpit@knoldus.com>
2015-02-27 15:06:21 +0530

    Better formatting and client side validation by Jquery validation

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -24,3 +24,122 @@ function search(){
     window.location.href = "/show?search="+search;
     
 }
+
+
+
+/**
+ * Validation for Sign In Form
+ */
+(function($, W, D) {
+	var JQUERY4U = {};
+
+	JQUERY4U.UTIL = {
+		setupFormValidation : function()
+		{
+			//form validation rules
+			$("#logInForm").validate(
+							{
+								rules : {
+									emailid : {
+										required : true,
+										email : true
+									},
+									password : {
+										required : true,
+										minlength : 5
+									},
+								},
+								messages : {
+									password : {
+										required : "Please provide a password",
+										minlength : "Password must be at least 5 char long"
+									},
+									emailid : "Please enter a valid email address",
+								},
+								submitHandler : function(
+										form) {
+									form.submit();
+								}
+							});
+		}
+	}
+
+	//when the dom has loaded setup form validation rules
+	$(D).ready(function($) {
+		JQUERY4U.UTIL.setupFormValidation();
+	});
+
+})(jQuery, window, document);
+
+
+
+(function($,W,D)
+        {
+            var JQUERY4U = {};
+
+            JQUERY4U.UTIL =
+            {
+                setupFormValidation: function()
+                {
+                    //form validation rules
+                    $("#signUpForm").validate({
+                        rules: {
+                            name:{
+                                required: true,
+                            },
+                            address:{
+                                required: true,
+                            },
+                            company:{
+                                required: true,
+                            },
+                            emailid: {
+                                required: true,
+                                email: true
+                            },
+                            password: {
+                                required: true,
+                                minlength: 5
+                            },
+                            phone:{
+                                required: true
+                            }
+                            
+                        },
+                        messages: {
+                            name:{
+                                required: "Please provide a name",
+                            },
+                            address:{
+                                required: "Please provide an address",
+                            },
+                            company:{
+                                required: "Please provide a company",
+                            },
+                            password: {
+                                required: "Please provide a password",
+                                minlength: "Password must be at least 5 char long"
+                            },
+                            emailid: {
+                                required: "Please enter an email address",
+                                email: "Please enter a valid email address"
+                            },
+                            phone:{
+                                required: "Please enter a phone number."
+                                
+                            }
+                             
+                        },
+                        submitHandler: function(form) {
+                            form.submit();
+                        }
+                    });
+                }
+            }
+
+            //when the dom has loaded setup form validation rules
+            $(D).ready(function($) {
+                JQUERY4U.UTIL.setupFormValidation();
+            });
+
+        })(jQuery, window, document);
